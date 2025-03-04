e7f3c724583e32d9b82e844c5b4a5088fa87b6f4
XenonIsAwesome <ofek171202@gmail.com>
2025-03-04 23:09:05 +0200

    update name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="leet",
-    version="0.1.0",
+    name="leetcode-cli",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "gql==3.5.1",
